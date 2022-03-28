9830fe325c176ccaa14d07a8ed7161b105f8e382
Joseph Alt <josephsonyalt@gmail.com>
2022-03-28 08:43:17 +0530

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.onload = function() {
+//     location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+// }
+function nggyu(){
+        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+setTimeout(() => {nggyu()}, 1500);
